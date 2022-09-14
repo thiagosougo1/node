3840e83280afb5fdd6aa09ac42099ec09f8f014c
thiagosougo1 <thiagosg@ciandt.com>
2022-09-13 22:23:05 -0300

    Commit

--- 6_EXPRESS/2_setup/index.js
+++ 6_EXPRESS/2_setup/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 3000; //variavel ambiente
+const port = 3000;
 
 const path = require("path");
+
 const basePath = path.join(__dirname, "templates");
+
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+
+  //Leituras da tabela users, resgatar usuário do banco
+  console.log(`Estamos buscando pelo usuário: ${id}`);
+
+  res.sendFile(`${basePath}/users.html`);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(`${basePath}/index.html`);
 });
 
 app.listen(port, () => {
-  console.log(`App rodando na porta ${port}`);
+  console.log(`App rodando na porta:${port}`);
 });
